ae33995ba6efeb3963f67c2e3e8c0bd10d592a38
akihon <398ah16@gmail.com>
2021-09-25 00:03:28 +0900

    lint

--- src/main/java/data/dto/LinkDto.java
+++ src/main/java/data/dto/LinkDto.java
@@ -3,12 +3,13 @@ package data.dto;
 /**
  * LinkDto is dto of network's link.
  *
- * @param weight the link's weight (length)
- * @param capacity the link's capacity
+ * @param weight          the link's weight (length)
+ * @param capacity        the link's capacity
  * @param headVertexIndex head vertex of the link
  * @param tailVertexIndex tail vertex of the link
- * @param label label
+ * @param label           label
  */
 public record LinkDto(
-        double weight, double capacity, int headVertexIndex, int tailVertexIndex, String label
-) {}
+    double weight, double capacity, int headVertexIndex, int tailVertexIndex, String label
+) {
+}
